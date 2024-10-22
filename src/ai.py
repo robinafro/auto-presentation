fd9eca7c49e52ffc012f82a0b999bbec98beb91b
robinafro <oble.ro.2023@skola.ssps.cz>
2024-10-22 20:11:50 +0200

    Removed openai api

--- src/ai.py
+++ src/ai.py
@@ -1,22 +1,13 @@
-import dotenv, requests, os
-from openai import OpenAI
-from settings import KEY_ENV_NAME, ROTATOR_URL, SYSPROMPT_PATH, RESPONSE_CACHE_PATH, MODEL, MODEL_TEMPERATURE
+import requests, os
+from settings import (
+    KEY_ENV_NAME,
+    ROTATOR_URL,
+    SYSPROMPT_PATH,
+    RESPONSE_CACHE_PATH,
+    MODEL,
+    MODEL_TEMPERATURE,
+)
 
-dotenv.load_dotenv()
-
-def fetch_key():
-    # Get the value of KEY_ENV_NAME in .env
-    stuckinvim_key = os.environ.get(KEY_ENV_NAME)
-
-    assert stuckinvim_key != None, f"Please set {KEY_ENV_NAME} in the .env to your key rotator key!"
-
-    # Fetch the actual open ai key
-    response = requests.get(ROTATOR_URL.replace("%key%", stuckinvim_key))
-    
-    assert response.status_code == 200, "Invalid key rotator key!"
-    assert response.json().get("key", None) != None, "Key rotator returned invalid response!"
-
-    return response.json()["key"]
 
 def fill_sysprompt(**kwargs):
     # Read the system prompt from its path
@@ -32,42 +23,3 @@ def fill_sysprompt(**kwargs):
         sysprompt = sysprompt.replace(key_processed, str(value))
 
     return sysprompt
-
-def get_response(client, prompt, debug_cache=False):
-    # If the debug cache is enabled, use it instead of wasting tokens
-    if debug_cache and os.path.exists(RESPONSE_CACHE_PATH):
-        with open(RESPONSE_CACHE_PATH, "r") as f:
-            return f.read()
-
-    messages = [
-        {"role": "user", "content": prompt}
-    ]
-
-    chat_completion = client.chat.completions.create(
-        messages=messages,
-        model=MODEL,
-        temperature=MODEL_TEMPERATURE,
-    )
-
-    text = chat_completion.choices[0].message.content
-
-    with open(RESPONSE_CACHE_PATH, "w") as f:
-        f.write(text)
-
-    return text
-
-def create_client():
-    return OpenAI(
-        api_key=fetch_key()
-    )
-
-if __name__ == "__main__":
-    client = OpenAI(
-        api_key=fetch_key()
-    )
-
-    prompt = fill_sysprompt(subject="Martin Luther King", country="the Czech Republic", language="czech", num_slides="4")
-
-    response = get_response(client, prompt, debug_cache=True)
-
-    print(response)
